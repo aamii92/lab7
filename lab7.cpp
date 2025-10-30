a50577fbba7875a746185b8a47d41295c0710112
NYAMKA <130820914+Nyamka93@users.noreply.github.com>
2025-10-30 21:40:16 +0800

    update 4
    tailbaruudiig nemj oruulsan main hurtel busad classuud zasvarlasan

--- lab7.cpp
+++ lab7.cpp
@@ -3,12 +3,12 @@
 #include <cmath>
 using namespace std;
 
+// Эх класс 
 class Shape {
 public:
     int x, y;
     char* name;
-
-    // Эцэг классын байгуулагч
+    // Эх классын параметртэй байгуулагч
     Shape(const char* n, int xcoor, int ycoor) {
         cout << "Constructor of Shape is called" << endl;
         name = new char[strlen(n) + 1];
@@ -17,73 +17,78 @@ public:
         y = ycoor;
     }
 
-    // Эцэг классын устгагч
-    virtual ~Shape() {
+    // Эх классын устгагч
+     ~Shape() {
         delete[] name;
         cout << "Destructor of Shape is called" << endl;
     }
 };
-
+// TwoDShape классыг Shape-аас удамшуулж байна
 class TwoDShape : public Shape {
 public:
     float r;
 
-    //  TwoDShape-ийн байгуулагч — Shape-ийг дуудаж байна
+    // TwoDShape-ийн параметртэй байгуулагч — Shape-ийг дуудаж байна
     TwoDShape(const char* n, float length, int xcoor, int ycoor)
         : Shape(n, xcoor, ycoor) {
         cout << "Constructor of TwoDShape is called" << endl;
         r = length;
     }
-
+    // TwoDShape классын устгагч
     ~TwoDShape() {
         cout << "Destructor of TwoDShape is called" << endl;
     }
 };
 
-// -------------------- Circle --------------------
+//  Circle классыг TwoDShape-аас удамшуулж байна
 class Circle : public TwoDShape {
 public:
+    // Circle классын параметртэй байгуулагч — TwoDShape-ийг дуудаж байна
     Circle(const char* n, float length, int xcoor, int ycoor)
         : TwoDShape(n, length, xcoor, ycoor) {
         cout << "Constructor of Circle is called" << endl;
     }
-
+    // Circle классын устгагч
     ~Circle() {
         cout << "Destructor of Circle is called" << endl;
     }
-
+    // Талбайг олох функц
     float Area() { return 3.14 * r * r; }
 };
 
+// Square классыг TwoDShape-аас удамшуулж байна
 class Square : public TwoDShape {
 public:
+    // Square классын параметртэй байгуулагч — TwoDShape-ийг дуудаж байна
     Square(const char* n, float length, int xcoor, int ycoor)
         : TwoDShape(n, length, xcoor, ycoor) {
         cout << "Constructor of Square is called" << endl;
     }
-
+    // Square классын устгагч
     ~Square() {
         cout << "Destructor of Square is called" << endl;
     }
-
+    // Талбайг олох функц
     float Area() { return r * r; }
 };
 
-
+// Triangle классыг TwoDShape-аас удамшуулж байна
 class Triangle : public TwoDShape {
 public:
+  // Square классын параметртэй байгуулагч — TwoDShape-ийг дуудаж байна
     Triangle(const char* n, float length, int xcoor, int ycoor)
         : TwoDShape(n, length, xcoor, ycoor) {
         cout << "Constructor of Triangle is called" << endl;
     }
-
+    // Triangle классын устгагч
     ~Triangle() {
         cout << "Destructor of Triangle is called" << endl;
     }
-
+    // Талбайг олох функц
     float Area() { return (sqrt(3) / 4) * r * r; }
 };
 
+// main функц
 int main() {
     // Дүрсүүдийг утга оноож байна
     Circle c1("Circle1", 3, 0, 0);
