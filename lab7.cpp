927af5d0d3e263816df717c954586c75120c472e
NYAMKA <130820914+Nyamka93@users.noreply.github.com>
2025-10-30 20:47:29 +0800

    update2
    zarim zasah utgiig zassan

--- lab7.cpp
+++ lab7.cpp
@@ -7,8 +7,7 @@ class Shape {
 public:
     int x, y;
     char* name;
-
-    // Эцэг классын байгуулагч
+    // Эх классын параметртэй байгуулагч
     Shape(const char* n, int xcoor, int ycoor) {
         cout << "Constructor of Shape is called" << endl;
         name = new char[strlen(n) + 1];
@@ -17,8 +16,8 @@ public:
         y = ycoor;
     }
 
-    // Эцэг классын устгагч
-    virtual ~Shape() {
+    // Эх классын устгагч
+     ~Shape() {
         delete[] name;
         cout << "Destructor of Shape is called" << endl;
     }
@@ -28,7 +27,7 @@ class TwoDShape : public Shape {
 public:
     float r;
 
-    //  TwoDShape-ийн байгуулагч — Shape-ийг дуудаж байна
+    // TwoDShape-ийн байгуулагч — Shape-ийг дуудаж байна
     TwoDShape(const char* n, float length, int xcoor, int ycoor)
         : Shape(n, xcoor, ycoor) {
         cout << "Constructor of TwoDShape is called" << endl;
@@ -55,6 +54,7 @@ public:
     float Area() { return 3.14 * r * r; }
 };
 
+// -------------------- Square --------------------
 class Square : public TwoDShape {
 public:
     Square(const char* n, float length, int xcoor, int ycoor)
@@ -69,7 +69,7 @@ public:
     float Area() { return r * r; }
 };
 
-
+// -------------------- Triangle --------------------
 class Triangle : public TwoDShape {
 public:
     Triangle(const char* n, float length, int xcoor, int ycoor)
@@ -84,19 +84,22 @@ public:
     float Area() { return (sqrt(3) / 4) * r * r; }
 };
 
+// -------------------- Main --------------------
 int main() {
     Circle c1("Circle1", 3, 0, 0);
     Circle c2("Circle2", 5, 2, 2);
     Square s1("Square1", 4, 1, 1);
+    Square s2("Square2", 2,6,7);
     Triangle t1("Triangle1", 6, 2, 2);
+    Triangle t2("Triangle2", 4,9,2);
 
-    // Бүх дүрсүүдийг массивт хадгалж байна
-    TwoDShape* shapes[4] = { &c1, &c2, &s1, &t1 };
-    float areas[4] = { c1.Area(), c2.Area(), s1.Area(), t1.Area() };
+    // Бүх дүрсүүдийг массивт хадгалъя
+    TwoDShape* shapes[6] = { &c1, &c2, &s1, &s2, &t1, &t2 };
+    float areas[6] = { c1.Area(), c2.Area(), s1.Area(), s2.Area(), t1.Area(), t2.Area() };
 
-    // Талбайгаар эрэмбэлэх ( bubble sort)
-    for (int i = 0; i < 4; i++) {
-        for (int j = i + 1; j < 4; j++) {
+    // Талбайгаар эрэмбэлэх (simple bubble sort)
+    for (int i = 0; i < 6; i++) {
+        for (int j = i + 1; j < 6; j++) {
             if (areas[i] > areas[j]) {
                 swap(areas[i], areas[j]);
                 swap(shapes[i], shapes[j]);
@@ -105,7 +108,7 @@ int main() {
     }
 
     cout << "\n--- Талбайгаар эрэмбэлсэн жагсаалт ---\n";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 6; i++) {
         cout << shapes[i]->name << " -> Талбай = " << areas[i] << endl;
     }
 
